3c00df3159c6182ed9dff1216e9b294718c05099
AndyWang99 <sam.andy.wang@gmail.com>
2019-05-20 17:16:01 -0400

    basic scaffolding

---
+++ src/views/About.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="about">
+    <b-container class="p-4" fluid>
+      <b-row>
+        <b-col>
+          <b-row>
+            <b-col>TITLE</b-col>
+          </b-row>
+          <b-row>
+            <b-col>subtit</b-col>
+          </b-row>
+          <b-row>
+            <b-col>p1</b-col>
+          </b-row>
+          <b-row>
+            <b-col>p2</b-col>
+          </b-row>
+          <b-row>
+            <b-col>p3</b-col>
+          </b-row>
+        </b-col>
+        <b-col>IMAGE</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../assets/main.scss";
+@import "../assets/variables.scss";
+.about {
+  background-color: $background;
+  padding-left: 10%;
+  padding-right: 10%;
+}
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "about"
+});
+</script>
+
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,13 +1,18 @@
 <template>
-  <home></home>
+  <div>
+    <home></home>
+    <about></about>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Home from "./Home.vue";
+import About from "./About.vue";
 export default Vue.extend({
   components: {
-    Home
+    Home,
+    About
   }
 });
 </script>
