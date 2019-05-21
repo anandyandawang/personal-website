a1165e77d459b9272dd0e180f718fcc980a164a0
AndyWang99 <sam.andy.wang@gmail.com>
2019-05-21 00:13:35 -0400

    modal looks good, has all info from json

--- src/components/ProjectClicked.vue
+++ src/components/ProjectClicked.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="project-clicked">{{project.name}}</div>
+  <div class="project-clicked">
+    <b-container class="p-4" fluid>
+      <b-row>
+        <b-col class="mb-4 text-left">
+          <b-row>
+            <b-col>
+              <h2>{{project.name}}</h2>
+            </b-col>
+          </b-row>
+          <b-row class="mb-3">
+            <b-col>
+              <h3>{{project.type}}</h3>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <p>{{project.desc}}</p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <p>Checkout the GitHub repo here: {{project.github}}</p>
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-col class="text-right">
+          <img class="img-fluid project-image" :src="project.imagePath">
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <style lang="scss">
@@ -11,6 +41,10 @@
   padding-bottom: 5%;
   padding-left: 2%;
   padding-right: 2%;
+
+  .project-image {
+    min-width: 200px;
+  }
 }
 </style>
 
