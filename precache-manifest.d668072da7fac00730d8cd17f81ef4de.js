846eca32a00191b0c5e4a7d3edefb85bace9d0ec
Andy Wang <sam.andy.wang@gmail.com>
2021-12-31 03:40:30 -0500

    Updates

---
+++ precache-manifest.d668072da7fac00730d8cd17f81ef4de.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "e188af3369c2c77e04cf",
+    "url": "/app.css"
+  },
+  {
+    "revision": "e188af3369c2c77e04cf",
+    "url": "/app.js"
+  },
+  {
+    "revision": "4159531169e9282ab274",
+    "url": "/chunk-vendors.css"
+  },
+  {
+    "revision": "4159531169e9282ab274",
+    "url": "/chunk-vendors.js"
+  },
+  {
+    "revision": "c6d93c49fed4a803a0303ae6843406c0",
+    "url": "/img/ANDY.svg"
+  },
+  {
+    "revision": "eee3e3266dbc73769241d915b8a5d00e",
+    "url": "/img/logo2.svg"
+  },
+  {
+    "revision": "2396850e13facd71d7c185e1bdf8586a",
+    "url": "/img/hex.svg"
+  },
+  {
+    "revision": "32c623d0d6ad63888cd8528f90f6e29f",
+    "url": "/img/github.svg"
+  },
+  {
+    "revision": "22f3854e821cb08c40d86c5df9b0a6b1",
+    "url": "/img/fab.svg"
+  },
+  {
+    "revision": "b6476b1809dd21e1236da5bf4401f183",
+    "url": "/img/fab-close.svg"
+  },
+  {
+    "revision": "9aa86a38faf1f86872f3536f0c62ccde",
+    "url": "/img/linkedin.svg"
+  },
+  {
+    "revision": "be4ca4d529c30e7765d49dc1edcd4bfb",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "388d7459b3868a6e3be3778ee560c983",
+    "url": "/img/WANG.svg"
+  },
+  {
+    "revision": "65e1d0cd272a17ead45178d188b59cde",
+    "url": "/img/portrait.jpg"
+  },
+  {
+    "revision": "0886e3199cd4c1172ad68f80beaf9889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "363153364e58286fd0b333126d52a6b9",
+    "url": "/projects/yikes.webp"
+  },
+  {
+    "revision": "3fa18bf03bfbb63af4111fe81d4a93a1",
+    "url": "/projects/personal-website.png"
+  }
+];
